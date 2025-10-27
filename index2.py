da2921591c173051dbacb4549eff022ea311e150
Andrei8580 <138531052+Andrei8580@users.noreply.github.com>
2025-10-27 19:31:09 +0400

    Add "For all selected"

--- index2.py
+++ index2.py
@@ -1,3 +1,7 @@
+
+                                 !!!Only for the first selected object!!!
+
+
 import maya.cmds as cmds
 
 def cleanup_selected_asset():
@@ -20,5 +24,38 @@ def cleanup_selected_asset():
     cmds.xform(object_name, centerPivots=True)
     
     print(f"🎉 QA PASS: Объект {object_name} Ready for export!")
+
+cleanup_selected_asset()
+
+
+
+
+
+
+                                    !!!For all selected!!!
+
+
+import maya.cmds as cmds
+
+def cleanup_selected_asset():
+    """Resets transformations and centers the Pivot for ALL selected objects."""
+    
+    selection = cmds.ls(selection=True, type='transform') # Let's make sure that the transform nodes are selected.
+    
+    if not selection:
+        print("❌ TA error: No objects are selected. Select assets to clean.")
+        return
+
+    # We make Python go through EVERY object in the 'selection' list.
+    for object_name in selection: 
+    # Instead of taking only selection[0], we take object_name from each element of the list.
+
+        # 2. Cleaning
+        cmds.makeIdentity(object_name, apply=True, t=1, r=1, s=1)
+        cmds.xform(object_name, centerPivots=True)
+        
+        print(f"🎉 QA PASS: Объект {object_name} Ready for export!")
+    
+    print("✅ All selected assets have successfully passed QA cleaning.")
 
 cleanup_selected_asset()
